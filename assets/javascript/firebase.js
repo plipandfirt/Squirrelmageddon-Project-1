b6e3a3d9ccfc0ba2e4d038d75d31fc4657328eb7
Chris Gauthier <chrsgauthier5@gmail.com>
2018-11-03 08:35:13 -0400

    push login name to firebase

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-
-
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyCE_Q_Xvlf03zMMkJEdHzvc1oI1MHA4W24",
@@ -12,48 +9,4 @@ $(document).ready(function() {
     };
     firebase.initializeApp(config);
 
-    var database = firebase.database();
-    var playersRef = database.ref("players");
-    var username = "Guest";
-    console.log(username);
-
-    $("#submitButton").click(function () {
-        if ($("#loginName").val().trim() !== "") {
-            username = capitalize($("#loginName").val().trim());
-            $("#loginName").hide()
-            $("#loginName").hide()
-            $("#submitButton").hide()
-            $("#loginConfirm").text("Thank you " + username + " for logging into Hot or Not")
-            userlog();
-        }
-        
-    });
-
-    // listener for 'enter' in login name input
-    $("#submitButton").keypress(function (e) {
-        if (e.keyCode === 13 && $("#loginName").val().trim() !== "") {
-            username = capitalize($("#loginName").val().trim());
-            $("#loginName").hide()
-            $("#submitButton").hide()
-            $("#loginConfirm").text("Thank you " + username + " for logging into Hot or Not")
-            userlog();
-        }
-        
-    });
-
-    // Function to capitalize usernames
-    function capitalize(name) {
-        return name.charAt(0).toUpperCase() + name.slice(1);
-    }
-
-    function userlog (){
-
-    }
-
-
-
-
-
     
-
-}); //end of document ready
