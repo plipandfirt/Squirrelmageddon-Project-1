c608829dac397dfa808d05b4cbdb5ba1f826820d
Chris Gauthier <chrsgauthier5@gmail.com>
2018-11-05 17:22:09 -0500

    Login page changes, still working on Modal so alert as placeholder

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -6,44 +6,56 @@ $(document).ready(function () {
     var playersRef = database.ref('players');
     var username = "Guest";
     var loggedIn = [];
-
+    
     // Looks in firebase and tells you who is logged in already, and pushes those name to an array.
     users.on("child_added", function (childSnapshot) {
-    var getUsers = childSnapshot.val().loginName
-    loggedIn.push(getUsers);
+        var getUsers = childSnapshot.val().loginName
+        loggedIn.push(getUsers);
     });
+    console.log(loggedIn);
 
 
+
+    // Runs when user clicks submit button
     $("#submitButton").click(function () {
-
-        if ($("#loginName").val().trim() !== "") {
-            username = capitalize($("#loginName").val().trim());
-        }
-        if (loggedIn.indexOf(username) > 0){
-            alert("this login Name is taken , please choose another!")
-        }
-        else{
+        username = capitalize($("#loginName").val().trim());
+        if ($("#loginName").val().trim() !== "" && loggedIn.indexOf(username) === -1) {
             $("#loginName").hide()
             $("#loginName").hide()
             $("#submitButton").hide()
-            $("#loginConfirm").html("Thank you " + '<strong>' + username + '</strong>' + " for logging into Hot or Not")
+            $("#player").html("Thank you " + '<strong>' + username + '</strong>' + " for logging into Super Hero Hot or Not!")
             userlog();
+            setTimeout(function(){selectPage()}, 3000);
         }
+        else if ($("#loginName").val().trim() === "") {
+            alert("Please enter a login name")
+        }
+        else {
+            alert("This username is taken, please choose another one")
+            }
         
     });
 
     // listener for 'enter' in login name input
     $("#loginName").keypress(function (e) {
-
-        if (e.keyCode === 13 && $("#loginName").val().trim() !== "") {
+        if (e.keyCode === 13) {
             username = capitalize($("#loginName").val().trim());
-            $("#loginName").hide()
-            $("#submitButton").hide()
-            $("#loginConfirm").html("Thank you " + '<strong>' + username + '</strong>' + " for logging into Hot or Not")
-            userlog();
+
+            if ($("#loginName").val().trim() !== "" && loggedIn.indexOf(username) === -1) {
+                $("#loginName").hide()
+                $("#loginName").hide()
+                $("#submitButton").hide()
+                $("#player").html("Thank you " + '<strong>' + username + '</strong>' + " for logging into Super Hero Hot or Not!")
+                userlog();
+                setTimeout(function(){selectPage()}, 3000);
+            }
+            else if ($("#loginName").val().trim() === "") {
+                alert("Please enter a login name")
+            }
+            else {
+                alert("This username is taken, please choose another one")
+                }
         }
-
-
     });
 
     // Function to capitalize usernames
@@ -51,14 +63,17 @@ $(document).ready(function () {
         return name.charAt(0).toUpperCase() + name.slice(1);
     }
 
-
-
+    // function to grab put login information into Firebase
     function userlog() {
         playersRef.push({
             loginName: username,
             time: firebase.database.ServerValue.TIMESTAMP,
             active: "true"
         });
+    }
+    // if login name submit is successful, take user to select page
+    function selectPage (){
+        document.location.href = 'Select.html'
     }
 
 
