a193c47faf2165d9fda9dafe9588fb6c938ced68
Mike Galarneau <mbgalarneau@gmail.com>
2018-11-07 00:36:26 -0500

    changed wikipedia function calls to reflect their capabiltiy, added no-image tag to api call to supress image calls

--- assets/javascript/wikipedia.js
+++ assets/javascript/wikipedia.js
@@ -2,7 +2,7 @@
 // wikipedia.js -  wikipedia.org API - used to augment the Marvel API search by querying wikipedia.org for content
 //                 Note:  it provides two apis,  one for a general query for when the query is generalized and 
 //                        not specific page is in mind.   The second is a direct search to an exact known page.
-//                 
+//  Source:  https://www.mediawiki.org/wiki/API:Parsing_wikitext                 
 
 //---------------------------
 // performWikiSearch(topic) - uses the passed parameter to run a query against the wikipedia API
@@ -10,7 +10,7 @@
 //                          - this is good for those times when you aren't sure of what you are looking for
 //                          - wikipedia is open sourced and does not require an API key to query, bless their hearts.
 //-----------------------------
-function performWikiSearch(topic) {
+function performWikiTopicSearch(topic) {
 
     // First of two required ajaz calls to get the topics matching our topic search
     $.ajax({
@@ -19,6 +19,7 @@ function performWikiSearch(topic) {
         contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
+        noimages: true,
         success: function (data) {
             console.log("--------- performWikiSearch() response data ------------");
             console.log(data);
@@ -28,7 +29,7 @@ function performWikiSearch(topic) {
             $.each(data, function (i, item) {
                 if (i == 1) {
                     var searchData = item[0];
-                    WikiAPIGetContent(searchData);
+                    performWikiPageSearch(searchData);
                 }
             });
         },
@@ -39,11 +40,11 @@ function performWikiSearch(topic) {
 }
 
 //---------------------------
-// wikiAPIGetContent(search) - uses the passed parameter to run a *detail* query against the wikipedia API
+// performWikiPageSearch(search) - uses the passed parameter to run a *detail* query against the wikipedia API
 //                           - use this search when you are sure the search leads to a specific detail page
 //                           - note it removes all anchors, links and images, to just have the summary table left
 //-----------------------------
-function wikiAPIGetContent(search) {
+function performWikiPageSearch(search) {
 
     // Second call required ajaz calls to narrow our result to a specific wikipedia page
     $.ajax({
@@ -52,6 +53,7 @@ function wikiAPIGetContent(search) {
         contentType: "application/json; charset=utf-8",
         async: false,
         dataType: "json",
+        noimages: true,    // don't bring back any images
         success: function (data) {
 
             var markup = data.parse.text["*"];
